7debe376de33b30f8f7d36d092050c167fc74eaf
roydwiff <125837538+roydwiff@users.noreply.github.com>
2023-06-16 23:49:40 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 // routes.js
 const express = require('express');
 const router = express.Router();
-const { registerHandler, loginHandler, logoutHandler,getPlantById} = require('./handler');
+const { registerHandler, loginHandler, logoutHandler,getPlantById,savePlant,savedPlants} = require('./handler');
 const { verifyToken, verifyTokenExpiry } = require('./handler');
 
 // Register route
@@ -16,6 +16,12 @@ router.post('/logout', logoutHandler);
 router.get('/Tanaman/:id', verifyToken, verifyTokenExpiry,getPlantById,(req, res) => {
   res.json({ message: 'Access granted to protected route' });
 });
+
+// Rute untuk menyimpan tumbuhan
+router.post('/savePlant', verifyToken, verifyTokenExpiry, savePlant);
+
+// Rute untuk mendapatkan daftar tumbuhan yang disimpan
+router.get('/savedPlants', verifyToken, verifyTokenExpiry, getSavedPlants);
 
 
 module.exports = router;
